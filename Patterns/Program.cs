3a281bc7a846cfc1a2146d1cb4a8b8ab9628500e
Moamen Soroor <moamensoroor@gmail.com>
2020-04-02 12:06:19 +0200

    Add project files.

---
+++ Patterns/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+
+    class BuildDir
+    {
+        public static void Build()
+        {
+            // CreationalPatterns
+            string[] creational = {
+                 "SingletonPattern"
+                ,"PrototypePattern"
+                ,"FactoryMethodPattern"
+                ,"BuilderPattern"
+                ,"AbstractFactoryPattern"
+            };
+
+            // StructuralPatterns
+            string[] structural = {
+                 "ProxyPattern"
+                ,"FlyweightPattern"
+                ,"CompositePattern"
+                ,"BridgePattern"
+                ,"FacadePattern"
+                ,"DecoratorPattern"
+                ,"AdapterPattern"
+            };
+
+            // BehavioralPatterns
+            string[] behavioral = {
+                 "ObserverPattern"
+                ,"StrategyPattern"
+                ,"TemplateMethodPattern"
+                ,"CommandPattern"
+                ,"IteratorPattern"
+                ,"MementoPattern"
+                ,"StatePattern"
+                ,"MediatorPattern"
+                ,"ChainOfResponsibilityPattern"
+                ,"VisitorPattern"
+                ,"InterpreterPattern"
+            };
+
+
+
+
+        }
+
+    }
+}
